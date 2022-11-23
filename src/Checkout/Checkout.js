081089c116dc2eb4cf8f6ad6fd6ba3afa6f85c8a
blunts22 <63960535+blunts22@users.noreply.github.com>
2022-11-23 12:18:07 +0530

    error proof maybe

--- src/Checkout/Checkout.js
+++ src/Checkout/Checkout.js
@@ -1,9 +1,11 @@
+import { useStateValue } from "../StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 import "./Checkout.css";
 import Subtotal from "./Subtotal";
 
-import React from "react";
-
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
+
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -13,6 +15,16 @@ function Checkout() {
         ></img>
         <div>
           <h2 className="checkout__title">Your Shopping Basket</h2>
+
+          {basket.map((item) => {
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />;
+          })}
         </div>
       </div>
       <div className="checkout__right">
